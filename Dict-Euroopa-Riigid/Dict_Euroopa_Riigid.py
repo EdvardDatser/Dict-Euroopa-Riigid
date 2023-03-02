2d64ad281182ebb3685f6b0d34f55547ba27c665
Suddenly EdaK <datsered@live.com>
2023-03-01 22:47:02 -0800

    Completed

--- Dict-Euroopa-Riigid/Dict_Euroopa_Riigid.py
+++ Dict-Euroopa-Riigid/Dict_Euroopa_Riigid.py
@@ -1,12 +1,22 @@
 from Modul import *
 
-sonastlik = {}
+
+sonastik={}
+riik_pealinn = {}
+pealinn_riik ={}
+
 while True:
-    menu=int(input("\n0-Exit\n1-Read\n2-Kirjuta\n"))
-    if menu==0:
+    print(riik_pealinn)
+    menu=int(input("Начальный словарь - 1\nНайти страну/столицу - 2\nДобавить в словарь - 3\nИсправить в словаре - 4\nЗакрыть - 0\n5Игра\n"))
+    if menu == 0:
         break
-    if menu==1:
-        sonastlik=Read("riigid_pealinnad")
-    elif menu==2:
-        sonastlik=Kirjuta("riigid_pealinnad")
-    
+    elif menu == 1:
+        file("riigid_pealinnad.txt", sonastik, riik_pealinn, pealinn_riik)
+    elif menu == 2:
+        riigid_pealinn(riik_pealinn, pealinn_riik)
+    elif menu == 3:
+        file_add(riik_pealinn, pealinn_riik)
+    elif menu == 4:
+        file_change(riik_pealinn, pealinn_riik)
+    elif menu == 5:
+        game(riik_pealinn, pealinn_riik)
--- Dict-Euroopa-Riigid/Modul.py
+++ Dict-Euroopa-Riigid/Modul.py
@@ -1,21 +1,73 @@
+from random import *
 
-sonastlik = {}
-def Read(fail:dict):
-    file=open(fail,"r", enconding ="utf-8-sig")
+def file(f: dict, s: dict, r: dict, p: dict):
+    file=open(f, "r")
     for line in file:
-        k v=line.strip().split('-')
-        sonastlik[k.strip()] = v.strip()
-
-print(sonastlik)
-
-def Kirjuta(fail:dict):
-    file=open(fail,"r", encoding= "utf-8-sig")
-    for line in file:
-        k,v=line.strip().split('-')
-        sonastlik[k.strip()] = v.strip()
-    riik=input("Lisa riik: ")
-    pealinn=input("Tema pealinn: ")
-    sonastlik.update({riik:pealinn})
+        k, v=line.strip().split("-")
+        s[k.strip()] = v.strip()
+        r[k]=v
+        p[v]=k
     file.close()
-    print(sonastlik)
-    return sonastlik
+    return s, r, p
+
+def riigid_pealinn(r:dict, p:dict):
+    ans=int(input("Страна - 1, Столица - 2\n"))
+    if ans == 1:
+        riik = str(input("Название страны.\n"))
+        print(r[f"{riik}"])
+    elif ans == 2:
+        pealinn = str(input("Название столицы.\n"))
+        print(p[f"{pealinn}"])
+    return r, p
+
+def file_add(r:dict, p:dict):
+        riik=input("Запишите страну: ")
+        pealinn=input("Запишите столицу: ")
+        r.update({riik:pealinn})
+        p.update({pealinn:riik})
+        return r, p
+
+def file_change(r:dict, p:dict):
+        riik = str(input("Введите страну которую хотите исправить: "))
+        pealinn = str(input("Введите столицу которую хотите исправить: "))
+        r.pop(riik)
+        p.pop(pealinn)
+        riik2 = str(input("Введите новую страну: "))
+        pealinn2 = str(input("Введите новую столицу: "))
+        r.update({riik2: pealinn2})
+        p.update({pealinn2: riik2})
+        return r, p
+
+def game(r:dict, p:dict):
+    riik=list(r.keys())
+    pealinn=list(p.values())
+    ko=0
+    try:
+        n = int(input("Сколько вопросов желаете?\n"))
+    except:
+        print(TypeError)
+
+    k = int(input("Желаете называть столицы(1) или страны(2)?"))
+    for i in range(0,n):
+        if k==1:
+            rand=choice(riik)
+            ind=riik.index(rand)
+            print(pealinn[ind])
+            sona=input("Введите: ")
+            if sona in riik[ind]:
+                print("Правильно")
+                ko+=1
+            else:
+                print("Неверно")
+        elif k ==2:
+            rand=choice(pealinn)
+            ind=pealinn.index(rand)
+            print(riik[ind])
+            sona = input("Введите: ")
+            if sona in pealinn[ind]:
+                print("Верно")
+                ko+=1
+            else:
+                print("Неверно")
+    percent = ko * 100 // n
+    print(percent, "%")
